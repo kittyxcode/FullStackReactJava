4d8d03bb8696f94baa718f0be17e400e3685076c
kittyxcode <katherine.pino059@gmail.com>
2024-02-21 22:30:41 -0300

    Add Project BackEnd

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # FullStackReactJava
 Un proyecto que consume una API REST con su backend, exponiendo un endpoint para que el frontend pueda consumirlo.
+
+Frontend (Next.Js, React, JS)
+    |
+    |--- Mi propia API REST(Spring Boot)
+    |
+Backend (Spring Boot, Java)
+    |
+    |--- API REST de Anime (Jikan)
+    |
+API ANIME
+
+
---
+++ backend/src/main/java/com/FullStackReactJava/backend/controllers/AnimeController.java
@@ -0,0 +1,18 @@
+package com.FullStackReactJava.backend.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class AnimeController {
+
+    @GetMapping("/anime/{id}")
+    public ResponseEntity<String> getAnime(@PathVariable String id) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://api.jikan.moe/v4/anime/" + id;
+        return restTemplate.getForEntity(url, String.class);
+    }
+}
